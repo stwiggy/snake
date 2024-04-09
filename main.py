dc64ef341fe3ecfc8598680308aa51abd5c0c856
stwiggy <144397102+stwiggy@users.noreply.github.com>
2024-04-09 01:35:44 -0700

    boundaries and "food"

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import time
 
 pygame.init()
 
@@ -7,12 +9,22 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50))
+player_size = 50
+player = pygame.Rect((0, 550, player_size, player_size))
+
+food_size = 20
+food = pygame.Rect((random.randint(0, 780), random.randint(0, 580), food_size, food_size))
+food_hidden = False
+
+def generateFood():
+    return pygame.Rect((random.randint(0, 780), random.randint(0, 580), food_size, food_size))
 
 run = True
 while run:
+
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    pygame.draw.rect(screen, (0, 255, 0), player)
+    
 
     key = pygame.key.get_pressed()
 
@@ -21,9 +33,31 @@ while run:
     elif key[pygame.K_d] == True:
         player.move_ip(1, 0)
     elif key[pygame.K_s] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_w] == True:
         player.move_ip(0, 1)
+    elif key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+
+    if player.left < 0:
+        player.left = 0
+    elif player.right > SCREEN_WIDTH:
+        player.right = SCREEN_WIDTH
+    if player.top < 0:
+        player.top = 0
+    elif player.bottom > SCREEN_HEIGHT:
+        player.bottom = SCREEN_HEIGHT
+
+    
+    if player.colliderect(food):
+        food_hidden = True
+
+    if food_hidden:
+        time.sleep(0.03)
+        food = generateFood()
+        food_hidden = False
+
+    else:
+        pygame.draw.rect(screen, (255, 0, 0), food)
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,3 +66,7 @@ while run:
     pygame.display.update()
 
 pygame.quit()
+
+#make a timer for food to be collect and respawn
+#make snake accelerate
+#make the snake grow
