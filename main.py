ddd4e33c0de5311f788b542562aa11c52a11f5ab
stwiggy <144397102+stwiggy@users.noreply.github.com>
2024-04-17 09:30:13 -0700

    Revert "boundaries and "food""
    This reverts commit dc64ef341fe3ecfc8598680308aa51abd5c0c856.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import pygame
-import random
-import time
 
 pygame.init()
 
@@ -9,78 +7,13 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player_width = 50
-player_height = 50
-player_speed = 1
-player_pos_x = 25
-player_pox_y = 575
-player_direction = "up"
-player_rotate_requested = False
-#player = pygame.Rect((player_pos_x, player_pox_y, player_width, player_height))
-player = pygame.image.load("snake-head.png").convert_alpha()
-
-def elongate():
-    global player_height, player
-    player_height += 10
-    player = pygame.Rect((player_pos_x, player_pox_y, player_width, player_height))
-    
-def rotatePlayer(direction, angle):
-    global player_direction, player_rotate_requested, player
-    if player_direction != direction:
-        if player_direction == "up":
-            player = pygame.transform.rotate(player, 0)
-        elif player_direction == "right":
-            player = pygame.transform.rotate(player, 0 - 90)
-        elif player_direction == "down":
-            player = pygame.transform.rotate(player, 0 - 180)
-        else:
-            player = pygame.transform.rotate(player, 0 - 270)
-    player_direction = direction
-    return player
-
-food_size = 20
-
-class Food(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((food_size, food_size))
-        self.image.fill((255, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(0, 780), random.randint(0, 580))
-
-        self.hitbox_size = 10
-        self.hitbox = pygame.Rect(self.rect.centerx - self.hitbox_size // 2, self.rect.centery - self.hitbox_size // 2, self.hitbox_size, self.hitbox_size)
-
-    def update(self):
-        self.hitbox.center = self.rect.center
-
-    def hide(self):
-        self.kill()
-
-def generateFood():
-    return Food()
-
-foods = pygame.sprite.Group()
-food = Food()
-foods.add(food)
+player = pygame.Rect((300, 250, 50, 50))
 
 run = True
 
 while run:
     screen.fill((0, 0, 0))
-    player_rect = player.get_rect(center= (player_pos_x, player_pox_y))
-    screen.blit(player, player_rect)
-
-    foods.update()
-    foods.draw(screen)
-
-    if food.hitbox.colliderect(player.get_rect()):
-        food.hide()
-        #elongate()
-        food = Food()
-        foods.add(food)
-
-    #pygame.draw.rect(screen, (0, 255, 0), player)
+    pygame.draw.rect(screen, (255, 0, 0), player)
 
     key = pygame.key.get_pressed()
 
@@ -95,23 +28,9 @@ while run:
         player = rotatePlayer("right", 0)
 
     elif key[pygame.K_s] == True:
-        player_pox_y += player_speed
-        player = rotatePlayer("down", 90)
-
+        player.move_ip(0, -1)
     elif key[pygame.K_w] == True:
-        player_pox_y -= player_speed
-        player = rotatePlayer("up", 180)
-
-
-    if player_pos_x < 0:
-        player_pos_x = 0
-    elif player_pos_x + player_width > SCREEN_WIDTH:
-        player_pos_x = SCREEN_WIDTH - player_width
-
-    if player_pox_y < 0:
-        player_pox_y = 0
-    elif player_pox_y + player_height > SCREEN_HEIGHT:
-        player_pox_y = SCREEN_HEIGHT - player_height
+        player.move_ip(0, 1)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -120,9 +39,3 @@ while run:
     pygame.display.flip()
 
 pygame.quit()
-
-#i put graphics
-#tmr: WHAT THE HECK IS GOING ON WITH ROTATION LOGIC
-#weird bounds
-#make collisions
-#growth
